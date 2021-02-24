c9ad3b449437cd00f7a5d44a1c0dc2db5cecff0e
miller barahona <millermiller01@hotmail.com>
2021-02-24 13:31:05 -0500

    david hexa hpta

--- src/Modelo/DAO/PlatoDAO.java
+++ src/Modelo/DAO/PlatoDAO.java
@@ -46,6 +46,7 @@ public class PlatoDAO {
                         System.out.println("bien jeje");
                     }
                 }
+                
                 return true;
             }
         } catch (SQLException ex) {
